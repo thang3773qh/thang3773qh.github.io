207bf587127844b8dabd948035834e30c9e6b676
thang3773qh <73017623+thang3773qh@users.noreply.github.com>
2024-05-08 12:13:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ answerButton.style.backgroundColor='#e52c45';
     }
 
     document.getElementById('nextButton').addEventListener('click', () => {
-      document.getElementById('numQuestions').innerHTML = currentQuestionIndex+"/"+data.length;
+      document.getElementById('numQuestion').innerHTML = currentQuestionIndex+"/"+data.length;
       if(currentQuestionIndex>=data.length-1) {
         alert(poin+"/"+data.length);
       }else{
